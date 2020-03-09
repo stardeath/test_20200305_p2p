cfa3db0c68f987be0742fa3094228c8b776213e9
stardeath <stardeath@hotmail.fr>
2020-03-09 23:13:38 +0100

    add a client to test batch

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
 start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "server0@127.0.0.1:5000"
 start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "client0@127.0.0.1:5010" "server0@127.0.0.1:5000"
 start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "client1@127.0.0.1:5011" "server0@127.0.0.1:5000"
+start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "client2@127.0.0.1:5012" "client1@127.0.0.1:5011"
