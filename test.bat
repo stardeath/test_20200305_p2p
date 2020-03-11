71f0292ad94717dbeee9a1599775a8f9d8f72151
stardeath <stardeath@hotmail.fr>
2020-03-11 22:24:49 +0100

    add release exe and update batch file to launch release binary

--- test.bat
+++ test.bat
@@ -1,8 +1,8 @@
-rem this test file makes it easy to spawn 4 instances of the debug build
+rem this test file makes it easy to spawn 4 instances of the release build
 rem 3 "clients" and 1 "server"
 rem client "0" and "1" with a connection to server "0" and client "2" connected to client "1"
 
-start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "server0@127.0.0.1:5000"
-start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "client0@127.0.0.1:5010" "server0@127.0.0.1:5000"
-start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "client1@127.0.0.1:5011" "server0@127.0.0.1:5000"
-start .\test_20200305_p2p\bin\Debug\test_20200305_p2p.exe "client2@127.0.0.1:5012" "client1@127.0.0.1:5011"
+start .\test_20200305_p2p\bin\Release\test_20200305_p2p.exe "server0@127.0.0.1:5000"
+start .\test_20200305_p2p\bin\Release\test_20200305_p2p.exe "client0@127.0.0.1:5010" "server0@127.0.0.1:5000"
+start .\test_20200305_p2p\bin\Release\test_20200305_p2p.exe "client1@127.0.0.1:5011" "server0@127.0.0.1:5000"
+start .\test_20200305_p2p\bin\Release\test_20200305_p2p.exe "client2@127.0.0.1:5012" "client1@127.0.0.1:5011"
